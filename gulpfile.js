a3bd2e0106d7765d243d0307f0f4e18aafe9a65a
Your Namemargo-lee <margo.lee@hyweb.com.tw>
2022-02-08 15:44:15 +0800

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+var gulp = require("gulp");
+(sass = require("gulp-sass")), (postcss = require("gulp-postcss"));
+autoprefixer = require("autoprefixer");
+var sourcemaps = require("gulp-sourcemaps");
+var browserSync = require("browser-sync").create();
+cssbeautify = require("gulp-cssbeautify");
+var beautify = require("gulp-beautify");
+
+//_______ task for scss folder to css main style
+gulp.task("watch", changeScss);
+function changeScss() {
+  console.log("Command executed successfully compiling SCSS in assets.");
+  return gulp
+    .src("2022_wda/assets/scss/**/*.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write(""))
+    .pipe(gulp.dest("2022_wda/assets/css"))
+    .pipe(
+      browserSync.reload({
+        stream: true,
+      })
+    );
+}
+const { watch } = require("gulp");
+//______監聽 sass更動
+watch(["2022_wda/assets/scss/**/*.scss"], gulp.parallel(changeScss));
+
+//_______task for style-dark
+gulp.task("dark", function () {
+  return gulp
+    .src("2022_wda/assets/css/style-dark.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("2022_wda/assets/css"));
+});
+
+//_______task for style-transparent
+gulp.task("transparent", function () {
+  return gulp
+    .src("2022_wda/assets/css/style-transparent.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("2022_wda/assets/css"));
+});
+
+//_______task for skinmodes
+gulp.task("skin", function () {
+  return gulp
+    .src("2022_wda/assets/css/skin-modes.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("2022_wda/assets/css"));
+});
